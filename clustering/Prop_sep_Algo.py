08248f90cc44bca69fc02ca15f028fe8cc0eb3e0
git-Francesco2014 <fr.casola@gmail.com>
2018-01-08 23:15:33 +0100

    Basic routines completed

--- clustering/Prop_sep_Algo.py
+++ clustering/Prop_sep_Algo.py
@@ -20,9 +20,9 @@ import warnings
 import numpy as np
 from joblib import Parallel, delayed
 from scipy.special import betainc
-import pdb
+import progressbar 
 
-
+np.warnings.filterwarnings('ignore')
 ###############################################################################
 # Main class defining Propagation-separation implementations
 
@@ -94,7 +94,7 @@ class Prop_sep_class():
             #finding the h0(xi) value, sample-dependent h0
             #a sample dependent radius containing at least n0 points 
             #within each xi, besides xi itself
-            h0xi = np.sort(D,axis=1)[:,n0]
+            h0xi = np.sort(D,axis=1)[:,n0-1]
             
             #finding the matrix Mij of the pairwise comparisons max(h0(xi),h0(xj)),
             #used for the initialization of w0ij
@@ -110,7 +110,7 @@ class Prop_sep_class():
             h0 = np.min(h0xi)
             
         #returning
-        return w0ij,h0
+        return w0ij,h0,h0xi
         
         
     def _initialize_radii_set(self,D,n_features,h0):
@@ -212,14 +212,17 @@ class Prop_sep_class():
         #put zeros on the diagonal to avoid the l=i,j terms
         np.fill_diagonal(wij_km1,0)
         
-        #must be within the sphere
+        #must be within the sphere too
+        #(within the sphere AND in the same cluster)
         wij_km1 = wij_km1*(D<=hk_l)
         
         #take all pairwise scalar products, as in section 2.1
         N_i_intersect_j = np.dot(wij_km1,wij_km1.T)
         
+        N_i_intersect_j2 = np.dot(wij_km1,(D<=hk_l).T)
+
         #return
-        return N_i_intersect_j
+        return N_i_intersect_j,N_i_intersect_j2
     
     def _estimate_N_i_comp_j(self,D,hk_l,wij_km1):
         """Routine to estimate the N_i_complem_j matrix, 
@@ -272,36 +275,20 @@ class Prop_sep_class():
         #return
         return q_ij
     
+    
     def _evaluate_KL(self,theta_ij,q_ij):
         """Function evaluating the Kullback-Leibler (KL) divergence
         """
         
-        #evaluating the divergence
-        KL_ij = theta_ij*np.log(theta_ij/q_ij) + (1 - theta_ij)*np.log((1-theta_ij)/(1-q_ij))
+        #evaluating the KL divergence (we are using here the symmetrized version)
+        KL_ij = (-1)**(theta_ij>q_ij)*(theta_ij-q_ij)*np.log((theta_ij*(1-q_ij))/(q_ij*(1-theta_ij)))        
         
-        #removing possible nans
-        KL_ij[np.isnan(KL_ij)] = 0
-        
-        #if q_ij or theta_ij is zero, points are not connected and 
-        #test should not be passed
-        KL_ij[q_ij==0] = np.inf
-        KL_ij[theta_ij==0] = np.inf
+        #replace pathological cases with numbers
+        KL_ij = np.nan_to_num(KL_ij)
         
         #return
         return KL_ij
-    
-    
-    def _heaviside(self,x,val=0.5):
-        """Custom definition of the Heaviside function.
-        Allows >= vs > and element-wise implementation.
-        """
-        
-        y = np.ones_like(x, dtype=np.float32)
-        y[x < 0.0] = 0.0
-        y[x == 0.0] = val
-        
-        #return
-        return y    
+      
          
     def _summarize_results(self,wij_final):
         """Routine that gets the final connectivity matrix and returns the 
@@ -348,8 +335,8 @@ class Prop_sep_class():
         #return the list of labels. Distances from cluster centers and
         #cluster errors not defined in this algorithm
         return (labels_,None,None)
-            
-            
+    
+    
     def run_AWC(self,D,n_features):
         """Routine running the Adaptive Weights Clustering (AWC) algorithm
         
@@ -384,78 +371,98 @@ class Prop_sep_class():
         #Initializing the weights matrix wij (page 9 of arXiv:1709.09102v1)
         if self.verbose>=0:
             print("Initializing the connectivity matrix.")
-        wij_mat,h0 = self._initialize_weights(D,n_features)
+        wij_mat,h0,h0xi = self._initialize_weights(D,n_features)
+        
     
         #Initializing the sequence of radii hk used in AWC
         if self.verbose>=0:
             print("Initializing the sequence of radii.")
+        #hk_list = self._initialize_radii_set(D,n_features,h0)
         hk_list = self._initialize_radii_set(D,n_features,h0)
         
-        #hk_list=[]
-        #hk_list=[0.61032778078668493, 1.0124228365658279, 1.0547511554864499, 1.1280514172678491, 1.1768602295939818, 1.2419742348374214, 1.2903487900563932, 1.3583077707206122, 1.4150971698084915, 1.4916433890176286, 1.5572411502397434, 1.6278820596099706, 1.7066048165875998, 1.7613914953808532, 1.9006577808748204, 1.960867155112759, 2.0862646045025057, 2.2549944567559366, 2.4052026941611389, 2.7018512172212548, 3.0364452901377956, 3.4820970692960302, 3.871046370169184, 4.7206991007688686, 5.500909015790028, 6.3674955830373383, 7.2953752473741895, 8.3043663213998453, 9.4764180996830234, 10.581233387464808, 11.987702031665622, 13.951702405083044, 17.0018381359193, 20.271408436514712, 23.816223462169653, 29.194391584686265, 38.815460837145814]
         #loop over the selected radii
         if self.verbose>=0:
             print("Starting the loop over the sequence of radii.")
             
-            
-        for id_h,hk_l in enumerate(hk_list[:len(hk_list)-1]):
+        #starting a progress bar            
+        progress = progressbar.ProgressBar()            
+        
+        #for id_h,hk_l in enumerate(hk_list[:len(hk_list)-1]):
+        for id_h in progress(range(1, len(hk_list))):
 
             #notify
             if self.verbose>=1:
                 print("Loop over the radius %d out of %d."%(id_h,len(hk_list)-1))
             
-            #estimate the N_i_intersect_j matrix, numerator of eq. 2.1
-            N_i_intersect_j = self._estimate_N_i_and_j(D,hk_l,wij_mat.copy())
+            #Counting points
+            #estimate the N_i_intersect_j matrix, numerator of eq. 2.1.
+            #it's a dense intersection (within the sphere AND in the same cluster), and not
+            N_i_intersect_j,N_i_intersect_j_loose = self._estimate_N_i_and_j(D,hk_list[id_h-1],wij_mat.copy())
         
             #estimate the N_i_complement_j matrix, denominator of eq. 2.1
-            N_i_complem_j = self._estimate_N_i_comp_j(D,hk_l,wij_mat)
+            N_i_complem_j = self._estimate_N_i_comp_j(D,hk_list[id_h-1],wij_mat)
         
             #define the mass of the union N_i_uni_j:
-            N_i_uni_j = N_i_intersect_j + N_i_complem_j
+            N_i_uni_j = N_i_intersect_j + N_i_complem_j     
             
-            print(10*'-')
-            print(N_i_intersect_j[-3:,0],N_i_uni_j[-3:,0])
-            
-            
-            #define the theta_ij matrix in eq. 2.1
-            with np.errstate(invalid='ignore'):
-                theta_ij = np.divide(N_i_intersect_j,N_i_uni_j)
-                theta_ij[N_i_uni_j==0] = 0
-                theta_ij = theta_ij.clip(min=0.05, max=0.9)
-        
+            #the theta_ij and q_ij terms in the paper. Start with q_ij
             #compute the qij matrix, defined in eq. 2.3
-            q_ij = self._compute_qij(D,hk_l,n_features)    
+            q_ij = self._compute_qij(D,hk_list[id_h-1],n_features)    
+            #removing complicated situations
+            q_ij = q_ij.clip(min=0.05, max=0.9)   
+            
+            #the theta_ij
+            theta_ij = np.divide(N_i_intersect_j,N_i_uni_j)
+            #handling a bunch of pathological cases
+            #handling situations with too many or too few points
+            #almost all points at the intersection of two spheres, around xi and xj
+            big_theta_ij = theta_ij >= 0.95
+            #if not points in the union, theta_ij is zero
+            theta_ij[N_i_uni_j == 0] = 0
+            #upper and lower clip. Both theta and q must be within [0,1]
+            theta_ij = theta_ij.clip(min=0.05, max=0.9)
             q_ij = q_ij.clip(min=0.05, max=0.9)
-        
-            #evaluating the Kullback-Leibler (KL) divergence
+            #almost no points at the intersection 
+            small_theta_ij = theta_ij <= 0.05
+            
+            #evaluate the KL divergence and compute the test statistics
             KL_ij = self._evaluate_KL(theta_ij,q_ij)
-            KL_ij[theta_ij<=0.05] = np.nan
-        
-            #evaluating Heaviside functions
-            H_ij = (self._heaviside(q_ij-theta_ij,1) - self._heaviside(theta_ij-q_ij,0))
+            #the test statistics
+            T_ij = N_i_uni_j*KL_ij                
             
-            #evaluating the test-statistics matrix Tij
-            T_ij = N_i_uni_j*KL_ij*H_ij
-            #remove points too far
-            T_ij[D>hk_l] = np.inf
+            #disconnecting points xi,xj where no points have been found at
+            #the intersections of their spheres.
+            T_ij[np.logical_or(small_theta_ij, N_i_intersect_j == 0)] = np.inf
+            #if theta_ij is very big, then set it to the cutoff lambda
+            T_ij[big_theta_ij] = self.lambda_ 
             
-            #update the wij matrix
-            #wij_mat = np.logical_and((D<=hk_list[id_h+1]),(T_ij<=self.lambda_)).astype(float)
-            logic_change = ((D<=hk_list[id_h+1])*(D>0)*(T_ij<=self.lambda_)*(np.isnan(T_ij) == False))
-            wij_mat[logic_change] = 1
-            np.fill_diagonal(wij_mat, 1)
+            """handling limits"""
+            #1 - do not disconnect, in wij_mat, 
+            #points that required an h0 bigger than the present radius
+            dont_disc = h0xi > hk_list[id_h-1]
+            T_ij[dont_disc, :] = np.inf
+            T_ij[:, dont_disc] = np.inf
             
-            print(10*'-')
-            #print(hk_l,T_ij[-3:,0],KL_ij[-3:,0],theta_ij[-3:,0],q_ij[-3:,0],sep='\n')
-            print(hk_l,N_i_intersect_j[-3:,0],N_i_uni_j[-3:,0],wij_mat[-3:,0],wij_mat[-3,0],sep='\n')            
+            #Matrix N_xixjexcl that shows the number of points around a point xj and within 
+            #the same cluster, provided the point is not xi
+            #1-Pij: points xj around xi and within the same cluster of xi
+            Pij =  (D <= hk_list[id_h-1])*wij_mat
+            #2-counting how many and removing xi
+            N_ij_xiexcl = (np.sum(Pij, axis = 1) - 1).reshape(1,-1)
+            #3-now counting removing xj
+            np.fill_diagonal(Pij, False)
+            N_xixjexcl = (np.tile(N_ij_xiexcl,(D.shape[0],1)) - Pij).T            
+            #don't change if points within intersection are too few
+            dont_disc2 = (N_xixjexcl == N_i_intersect_j_loose) * (N_i_intersect_j < 0.5 * N_i_intersect_j_loose)        
+            T_ij[np.logical_or(dont_disc2, dont_disc2.T)] = np.nan                        
+                
+            #applying eq. 2.6 of the paper
+            I = (D <= hk_list[id_h]) * (D > 0) * (T_ij != np.inf) * (np.isnan(T_ij) == False) 
+            wij_mat[I] = 1 * (T_ij[I] <= self.lambda_)
             
-            if id_h>5:
-                #pdb.set_trace()
-                labels_,cluster_distances_,cluster_error_ = self._summarize_results(wij_mat)
-                N_cluster_AWC = list(labels_.keys())
-                print(N_cluster_AWC)
-                if N_cluster_AWC[0]==15:
-                    break
+            wij_mat[np.isnan(T_ij)] = 0
+            #always put ones on the diagonal (a point is connected to himself)
+            np.fill_diagonal(wij_mat, 1)        
         
         #end of the loop
         if self.verbose>=0:
@@ -464,7 +471,9 @@ class Prop_sep_class():
         #Summary of the results
         labels_,cluster_distances_,cluster_error_ = self._summarize_results(wij_mat)
         
+        if self.verbose>=0:
+            print("Done.")
+        
         #return
         return (labels_,cluster_distances_,cluster_error_)
-
         
